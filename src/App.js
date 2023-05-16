8d748c2566dc905c8a30c2f550215bf972a0cc73
colin mcgovern <colinmcg@udel.edu>
2023-05-16 14:13:16 -0400

    adding persistance

--- src/App.js
+++ src/App.js
@@ -13,23 +13,61 @@ import ChangePassword from './components/auth/ChangePassword';
 import ChangePasswordConfirm from './components/auth/ChangePasswordConfirm';
 import Welcome from './components/auth/Welcome';
 import Footer from './components/Footer';
+import { Auth } from 'aws-amplify';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 library.add(faEdit);
 
 class App extends Component {
+
+  state = {
+    isAuthenticated: false,
+    isAuthenticating: true,
+    user: null
+  }
+
+  setAuthStatus = authenticated => {
+    this.setState({ isAuthenticated: authenticated });
+  }
+
+  setUser = user => {
+    this.setState({ user: user })
+  }
+
+  async componentDidMount() {
+    try{
+      const session = await Auth.currentSession();
+      this.setAuthStatus(true);
+      console.log(session);
+      const user = await Auth.currentAuthenticatedUser();
+      this.setUser(user);
+    }catch(error){
+      console.log(error);
+    }
+    this.setState({ isAuthenticating: false });
+  }
+
   render() {
+  
+    const authProps = {
+      isAuthenticated: this.state.isAuthenticated,
+      user: this.state.user,
+      setAuthStatus: this.setAuthStatus,
+      setUser: this.setUser
+    }
+
     return (
+      !this.state.isAuthenticating &&
       <div className="App">
         <Router>
           <div>
-            <Navbar />
+            <Navbar auth={authProps}/>
             <Switch>
-              <Route exact path="/" component={Home} />
+              <Route exact path="/" render={(props) => <Home {...props} auth={authProps} />} />
               <Route exact path="/products" component={Products} />
               <Route exact path="/admin" component={ProductAdmin} />
-              <Route exact path="/login" component={LogIn} />
-              <Route exact path="/register" component={Register} />
+              <Route exact path="/login" render={(props) => <LogIn {...props} auth={authProps} />} />
+              <Route exact path="/register" render={(props) => <Register {...props} auth={authProps} />} />
               <Route exact path="/forgotpassword" component={ForgotPassword} />
               <Route exact path="/forgotpasswordverification" component={ForgotPasswordVerification} />
               <Route exact path="/changepassword" component={ChangePassword} />
--- src/components/auth/ForgotPassword.js
+++ src/components/auth/ForgotPassword.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import FormErrors from "../FormErrors";
 import Validate from "../utility/FormValidation";
+import { Auth } from 'aws-amplify'
 
 class ForgotPassword extends Component {
   state = {
